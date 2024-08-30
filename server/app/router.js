3ab62c879078ed21d55270db308de8f5023c655c
Rossignol Alex <alexnw33910@gmail.com>
2024-08-30 10:49:26 +0200

    Updated router & tables for City table

--- server/app/router.js
+++ server/app/router.js
@@ -10,21 +10,25 @@ const router = express.Router();
 const items = require("./controllers/itemActions");
 const users = require("./controllers/UserActions");
 const artworks = require("./controllers/ArtworkActions");
+const citys = require("./controllers/CityActions");
 
 // Route to get a list of items
 router.get("/items", items.browse);
 router.get("/users", users.browse);
 router.get("/artworks", artworks.browse);
+router.get("/citys", citys.browse);
 
 // Route to get a specific item by ID
 router.get("/items/:id", items.read);
 router.get("/users/:id", users.read);
 router.get("/artworks/:id", artworks.read);
+router.get("/citys/:id", citys.read);
 
 // Route to add a new item
 router.post("/items", items.add);
 router.post("/users", users.add);
 router.post("/artworks", artworks.add);
+router.post("/citys", citys.add);
 
 /* ************************************************************************* */
 
--- server/database/tables.js
+++ server/database/tables.js
@@ -2,6 +2,7 @@
 const ItemRepository = require("./models/ItemRepository");
 const UserRepository = require("./models/UserRepository");
 const ArtworkRepository = require("./models/ArtworkRepository");
+const CityRepository = require("./models/CityRepository");
 
 // Create an empty object to hold data repositories for different tables
 const tables = {};
@@ -14,6 +15,7 @@ const tables = {};
 tables.item = new ItemRepository();
 tables.user = new UserRepository();
 tables.artwork = new ArtworkRepository();
+tables.city = new CityRepository();
 
 /* ************************************************************************* */
 
