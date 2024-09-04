f7bd9581f97a65a35d44fdb982318b5e647f9b60
Rosa Ben <benchabane.rosa@outlook.com>
2024-09-04 16:59:29 +0200

    added profile page, getUserById in request, added loader in main

--- client/src/services/request.js
+++ client/src/services/request.js
@@ -6,3 +6,10 @@ export default function getMap() {
     .then((response) => response.data)
     .catch((error) => console.info(error));
 }
+
+export function getUserbyId(id) {
+  return myAxios
+    .get(`/users/${id}`)
+    .then((response) => response.data)
+    .catch((error) => console.info(error));
+}
