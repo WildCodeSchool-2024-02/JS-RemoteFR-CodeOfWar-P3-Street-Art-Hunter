3459b754b04280316a699ce22b72cd2258d09a57
Davido91390 <david.kabore@hotmail.fr>
2024-09-04 11:34:08 +0200

    add app.css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,16 @@
+import "./styles/app.css";
+
 function App() {
-  return <h1>Hello Word</h1>;
+  return (
+    <>
+      <h1>Hello Word</h1>
+      <p>
+        ectetur, adipisicing elit. Facere quaerat quis quia nisi nihil beatae
+        dolores asperiores similique atque dolor adipisci consectetur unde culpa
+        inventore, doloribus accusantium dignissimos minima. Rerum.
+      </p>
+    </>
+  );
 }
 
 export default App;
