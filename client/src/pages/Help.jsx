018c5644bc789839b6319b4f95c638c3ceea141c
Alex Rossignol <alexrossignol@air-de-marlene.home>
2024-09-05 09:36:34 +0200

    Created help page & its functionalities 🚀

--- client/src/pages/Help.jsx
+++ client/src/pages/Help.jsx
@@ -1,3 +1,41 @@
+import { useState } from "react";
+
+import { Règles, Licence, Contact } from "../components/Helpers";
+
+import "../styles/styleHelp.css";
+
 export default function Help() {
-  return <h1>Coucou from Help</h1>;
+  const [isActive, setIsActive] = useState("Règles");
+
+  const helpers = [
+    { name: "Règles" },
+    { name: "Licence" },
+    { name: "Contact" },
+    { name: "" },
+  ];
+  const helpComponents = { Règles, Licence, Contact };
+  const Component = helpComponents[isActive];
+
+  return (
+    <section className="helpContainer">
+      <div className="helpHeader">
+        <h1 className="helpTitle">Helper</h1>
+        <div className="helpersContainer">
+          {helpers.map((help) => (
+            <button
+              key={help.name}
+              type="button"
+              className={help.name === isActive ? "helpers active" : " helpers"}
+              onClick={help.name.length > 0 && (() => setIsActive(help.name))}
+            >
+              <p>{help.name}</p>
+            </button>
+          ))}
+        </div>
+      </div>
+      <div className="helpMain">
+        <Component />
+      </div>
+    </section>
+  );
 }
