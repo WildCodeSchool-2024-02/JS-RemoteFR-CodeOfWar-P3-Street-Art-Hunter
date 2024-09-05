fed15fa66fc23c195d91fae260e3b13e6a824365
Davido91390 <david.kabore@hotmail.fr>
2024-09-05 17:52:52 +0200

    css modification

--- client/src/pages/Gallery.jsx
+++ client/src/pages/Gallery.jsx
@@ -1,31 +1,59 @@
 import { useLoaderData, Link } from "react-router-dom";
+import { useState } from "react";
 import NavBar from "../components/NavBar";
 
 import "../styles/gallery.css";
-import Filter from "../assets/images/gallery_filter.svg";
+// import Filter from "../assets/images/gallery_filter.svg";
 
 export default function Gallery() {
-  const gallery = useLoaderData();
+  const data = useLoaderData();
+  console.info(data);
+  // const [selection, setSelection] = useState([]);
 
-  console.info(gallery);
+  // const handleChangeSelection = (event) => {
+  //   setSelection(event.target.value);
+  // };
+  const [currentFilter, setCurrentFilter] = useState("");
+  const getFilter = (event) => setCurrentFilter(event.target.value);
+
+  // const filteredArtwork = data.filter((style) =>
+  //   selection === "" ? style : style.style === selection
+  // );
+
+  /* <input type="image" src={Filter} alt="Filtre" value="Filtres" />{" "}
+    Filtres */
+
   return (
     <section className="gallery">
       <section className="header-gallery">
         <h1>Galerie</h1>
         <label>
-          <input type="image" src={Filter} alt="Filtre" value="Filtres" />{" "}
-          Filtres
+          {" "}
+          <select onChange={getFilter} id="artwork-select">
+            <option value="Filtre">Filtres</option>
+            {data?.map((style) => (
+              <option key={style.id} value={style.style}>
+                {style.style}
+              </option>
+            ))}
+          </select>
         </label>
       </section>
       <section className="body-gallery">
         <div className="grid">
-          {gallery.map((image) => (
-            <div className="grid-item" key={image.id}>
-              <Link to={`/gallery/${image.id}`}>
-                <img src={image.image_url} alt={image.title} />
-              </Link>
-            </div>
-          ))}
+          {data
+            .filter((artwork) =>
+              currentFilter
+                ? artwork.style === currentFilter
+                : artwork.image_url
+            )
+            .map((artwork) => (
+              <div className="grid-item" key={artwork.id}>
+                <Link to={`/gallery/${artwork.id}`}>
+                  <img src={artwork.image_url} alt={artwork.title} />
+                </Link>
+              </div>
+            ))}
         </div>
       </section>
       <div className="galleryNavbar">
