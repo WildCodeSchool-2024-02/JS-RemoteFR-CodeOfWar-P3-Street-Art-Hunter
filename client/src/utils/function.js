6506aeae250b2da3880ceae8e750f29d2c83ee89
Davido91390 <david.kabore@hotmail.fr>
2024-09-11 21:26:32 +0200

    add a new file for functions

--- client/src/utils/function.js
+++ client/src/utils/function.js
@@ -1,3 +1,9 @@
+export const datePicture = new Date().toLocaleDateString("fr-FR", {
+  year: "numeric",
+  month: "numeric",
+  day: "numeric",
+});
+
 export function frenchDate(date) {
   const event = new Date(date);
   const options = {
@@ -9,9 +15,3 @@ export function frenchDate(date) {
     ? event.toLocaleDateString("fr-FR", options)
     : " Pas d'information";
 }
-
-export const datePicture = new Date().toLocaleDateString("fr-FR", {
-  year: "numeric",
-  month: "numeric",
-  day: "numeric",
-});
