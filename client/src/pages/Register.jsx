b7b996b7dd9f334d1ec6b3d41145246e3dba49bf
Davido91390 <david.kabore@hotmail.fr>
2024-09-17 16:32:18 +0200

    update state name

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -6,7 +6,7 @@ import logo from "../assets/images/logo_streetArt.svg";
 import GradientButton from "../components/GradientButton";
 
 export default function Register() {
-  const [userInscription, setUserInscription] = useState({
+  const [userRegistration, setUserRegistration] = useState({
     pseudo: "",
     mail: "",
     password: "",
@@ -15,7 +15,7 @@ export default function Register() {
 
   const handleChangeUser = (event) => {
     const { name, value } = event.target;
-    setUserInscription((previousAdd) => ({
+    setUserRegistration((previousAdd) => ({
       ...previousAdd,
       [name]: value,
     }));
@@ -26,9 +26,9 @@ export default function Register() {
   };
   const sendCredentials = (event) => {
     event.preventDefault();
-    if (userInscription.password === confirmPassword) {
+    if (userRegistration.password === confirmPassword) {
       myAxios
-        .post("/users", userInscription, { withCredentials: true })
+        .post("/users", userRegistration, { withCredentials: true })
         .then((response) => console.info(response))
         .catch((error) => console.error(error));
     } else {
@@ -46,40 +46,35 @@ export default function Register() {
             type="text"
             placeholder="Pseudo*"
             name="pseudo"
-            value={userInscription.pseudo}
+            value={userRegistration.pseudo}
             onChange={handleChangeUser}
           />
-
           <input
             type="email"
             name="mail"
             placeholder="Adresse mail*"
-            value={userInscription.mail}
+            value={userRegistration.mail}
             onChange={handleChangeUser}
           />
-
           <input
             type="password"
             name="password"
             placeholder="Mot de passe*"
-            value={userInscription.password}
+            value={userRegistration.password}
             onChange={handleChangeUser}
           />
-
           <input
             type="password"
             name="confirm_password"
             placeholder="Confirmez votre mot de passe*"
             onChange={handleChangePassword}
           />
-
           <GradientButton
             text="Créer un compte"
             type="submit"
             className="registerSubmit"
             onClick={sendCredentials}
           />
-
           <p className="linkRegister">
             Déjà un compte ?{" "}
             <Link to="/connection" className="InscriptionLink">
