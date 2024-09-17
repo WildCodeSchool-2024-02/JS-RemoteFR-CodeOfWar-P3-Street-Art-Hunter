ca5f65678dc2d9009527d7f15c438e316785ce40
Alex Rossignol <alexrossignol@air-de-marlene.home>
2024-09-17 20:42:21 +0200

    Added rules into app & main for window width ✈️

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,24 @@
 import { useLocation, Outlet } from "react-router-dom";
+import useScreenWidth from "./utils/hook/useScreenWidth";
 
 import { GeoLocationProvider } from "./services/context/GeoLocationContext";
+
+import NavBarDesktop from "./components/desktop/NavBarDesktop";
 import NavBar from "./components/NavBar";
 
 import "./styles/app.css";
 
 function App() {
   const location = useLocation();
+  const screenWidth = useScreenWidth();
+
+  const NavResponsive = screenWidth <= 480 ? <NavBar /> : <NavBarDesktop />;
 
   return (
     <>
       {location.pathname === "/connection" ||
       location.pathname === "/inscription" ? null : (
-        <NavBar />
+        <NavResponsive />
       )}
       <GeoLocationProvider>
         <main>
