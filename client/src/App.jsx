b416a39888182ed1c33ead1bb094d51f211e28e7
Alex Rossignol <alexrossignol@air-de-marlene.home>
2024-09-18 10:12:24 +0200

    Fixed probleme with NavBarDesktop

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,32 +1,20 @@
 import { useLocation, Outlet } from "react-router-dom";
-import useScreenWidth from "./utils/hook/useScreenWidth";
 
 import { GeoLocationProvider } from "./services/context/GeoLocationContext";
 
-import NavBarDesktop from "./components/desktop/NavBarDesktop";
 import NavBar from "./components/NavBar";
 
 import "./styles/app.css";
 
 function App() {
   const location = useLocation();
-  const screenWidth = useScreenWidth();
 
   return (
     <>
-      {screenWidth <= 480 ? (
-        <>
-          (
-          {location.pathname === "/connection" ||
-          location.pathname === "/inscription" ? null : (
-            <NavBar />
-          )}
-          )
-        </>
-      ) : (
-        <NavBarDesktop />
+      {location.pathname === "/connection" ||
+      location.pathname === "/register" ? null : (
+        <NavBar />
       )}
-
       <GeoLocationProvider>
         <main>
           <Outlet />
--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import useScreenWidth from "../utils/hook/useScreenWidth";
 
 import Help from "../assets/images/nav_help.svg";
 import Gallery from "../assets/images/nav_gallery.svg";
@@ -6,10 +7,16 @@ import Map from "../assets/images/nav_map.svg";
 import Camera from "../assets/images/nav_camera.svg";
 import Profil from "../assets/images/nav_profil.svg";
 
+import NavBarDesktop from "./desktop/NavBarDesktop";
+
 import "../styles/navBar.css";
 
 export default function NavBar() {
-  return (
+  const screenWidth = useScreenWidth();
+
+  return screenWidth > 480 ? (
+    <NavBarDesktop />
+  ) : (
     <nav className="navBar">
       <div className="nav_picto">
         <Link to="/help">
