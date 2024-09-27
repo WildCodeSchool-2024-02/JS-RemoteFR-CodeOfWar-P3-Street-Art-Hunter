06ac29defec162d2579efc0590f507b803e349ac
Alex Rossignol <alexrossignol@air-de-marlene.home>
2024-09-27 11:25:36 +0200

    Fixed bug with key artwork_id 💪

--- client/src/services/request.js
+++ client/src/services/request.js
@@ -97,7 +97,7 @@ export function postArtwork(formData) {
 // *** FAVORITES *** \\
 export function postFavorites(artworkId) {
   myAxios
-    .post(`/favorites`, { artworkId }, { withCredentials: true })
+    .post(`/favorites`, { artwork_id: artworkId }, { withCredentials: true })
     .then((response) => console.info(response))
     .catch((error) => console.error(error));
 }
